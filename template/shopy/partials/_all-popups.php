05459e81ae72a4313251b21217edeacaea496ad6
CELEBRITY BASSIST <aiyedunmiracle956888@gmail.com>
2023-11-26 20:21:49 +0100

    first commit

---
+++ template/shopy/partials/_all-popups.php
@@ -0,0 +1,66 @@
+<!-- Ask Form -->
+<?php
+@@include('./popups/_ask-form-popup.html', {
+    "path": "..",
+})
+?>
+<!-- End Ask Form -->
+
+<!-- Header Search -->
+<?php
+@@include('./popups/_header-search-popup.html', {
+    "path": "..",
+})
+?>
+<!-- End Header Search -->
+
+
+<!-- Header Side Search -->
+<?php
+@@include('./popups/_header-search-side-popup.html', {
+    "path": "..",
+})
+?>
+<!-- End Header Side Search -->
+
+<!-- Login Popup  -->
+<?php
+@@include('./popups/_login-popup.html', {
+    "path": "..",
+})
+?>
+<!-- End Login Popup  -->
+
+<!-- Mini Cart  -->
+<?php
+@@include('./popups/_mini-cart-popup.html', {
+    "path": "..",
+})
+?>
+<!-- End Mini Cart  -->
+
+<!-- Quick View  -->
+<?php
+@@include('./popups/_quick-view-popup.html', {
+    "path": "..",
+})
+?>
+<!-- End Quick View  -->
+
+
+<!-- Sipping Popup  -->
+<?php
+@@include('./popups/_shipping-popup.html', {
+    "path": "..",
+})
+?>
+<!-- End Sipping Popup  -->
+
+
+<!-- Size Chart  -->
+<?php
+@@include('./popups/_size-chart-popup.html', {
+    "path": "..",
+})
+?>
+<!-- End Size Chart  -->
---
+++ template/shopy/partials/footer/_footer-04.php
@@ -0,0 +1,101 @@
+<!-- Footer -->
+<footer class="bg-dark footer footer-border-top light">
+    <div class="footer-top py-6">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-6 col-lg-3 my-3">
+                    <h6 class="text-uppercase mb-5 text-white">
+                        Support
+                    </h6>
+                    <ul class="list-unstyled white-link footer-link-1 m-0">
+                        <li class="pb-2">
+                            <a href="#">Help</a>
+                        </li>
+                        <li class="pb-2">
+                            <a href="#">Contact Us</a>
+                        </li>
+                        <li class="pb-2">
+                            <a href="#">Feedback</a>
+                        </li>
+                        <li class="pb-2">
+                            <a href="#">Unsubscribe</a>
+                        </li>
+                        <li class="pb-2">
+                            <a href="#">Reservations</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-sm-6 col-lg-3 my-3">
+                    <h6 class="text-uppercase mb-5 text-white">
+                        Policies
+                    </h6>
+                    <ul class="list-unstyled white-link footer-link-1 m-0">
+                        <li class="pb-2">
+                            <a href="#">Privacy Policy</a>
+                        </li>
+                        <li class="pb-2">
+                            <a href="#">Terms of use</a>
+                        </li>
+                        <li class="pb-2">
+                            <a href="#">Gift card conditions</a>
+                        </li>
+                        <li class="pb-2">
+                            <a href="#">Shipping</a>
+                        </li>
+                        <li class="pb-2">
+                            <a href="#">Return</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-sm-6 col-lg-3 my-3">
+                    <h6 class="text-uppercase mb-5 text-white">
+                        Categories
+                    </h6>
+                    <ul class="list-unstyled white-link footer-link-1 m-0">
+                        <li class="pb-2">
+                            <a href="#">Men</a>
+                        </li>
+                        <li class="pb-2">
+                            <a href="#">Women</a>
+                        </li>
+                        <li class="pb-2">
+                            <a href="#">Accessories</a>
+                        </li>
+                        <li class="pb-2">
+                            <a href="#">Shoes</a>
+                        </li>
+                        <li class="pb-2">
+                            <a href="#">Denim</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-sm-6 col-lg-3 my-3">
+                    <h6 class="text-uppercase mb-5 text-white">
+                        Stay up to date
+                    </h6>
+                    <p class="text-white text-opacity-85">Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan.</p>
+                    <form>
+                        <div class="input-group">
+                            <input type="email" class="form-control" placeholder="Enter your email">
+                            <button class="btn btn-primary" type="submit"><i class="bi bi-envelope"></i></button>
+                        </div>
+                        <p class="m-0 text-white text-opacity-85">New UI kits or big discounts. Never spam.</p>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer-bottom py-3 footer-border-top light small">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 text-center text-md-start py-1">
+                    <p class="m-0 text-white text-opacity-85">© 2023 copyright by <a href="https://www.pxdraft.com/" target="_blank" class="text-reset">pxdraft</a></p>
+                </div>
+                <div class="col-md-6 text-center text-md-end py-1">
+                    <img src="@@imgPath/img/payments.png" title="" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<!-- End Footer -->
